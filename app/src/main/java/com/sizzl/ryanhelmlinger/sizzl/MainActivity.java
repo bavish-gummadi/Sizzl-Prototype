ac660f716bb298b0944e6664edb78884eba18a77
ryanjhelmlinger <ryan.helmlinger81@gmail.com>
2018-02-12 17:45:03 -0500

    Initial commit

---
+++ app/src/main/java/com/sizzl/ryanhelmlinger/sizzl/MainActivity.java
@@ -0,0 +1,46 @@
+package com.sizzl.ryanhelmlinger.sizzl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    private void goToSecondActivity(){
+        Intent intent = new Intent(this, RestaurantsActivity.class);
+        startActivity(intent);
+    }
+}
